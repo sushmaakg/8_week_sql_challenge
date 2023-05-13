41dac555617c6f6cad6480a0065a64edecd9cf1a
sushmaakg <30415592+sushmaakg@users.noreply.github.com>
2023-05-13 16:44:15 +0530

    Delete Danny's Dinner.sql

--- Danny's Dinner.sql
+++
@@ -1,9 +0,0 @@
--- 1. What is the total amount each customer spent at the restaurant
-
-SELECT
-  	s.customer_id,sum(m.price)
-FROM dannys_diner.sales s
-inner join dannys_diner.menu m 
-on s.product_id=m.product_id
-group by s.customer_id
-;
